ce473c8fa8294775963ed6f6a8d3e61ac28f9d9a
Turkan <kasamanlit@gmail.com>
2022-09-21 16:15:31 -0400

    class Numbers and Finra tasks created

---
+++ src/test/week1/Finra.java
@@ -0,0 +1,7 @@
+package test.week1;
+
+public class Finra {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/week1/Numbers.java
@@ -0,0 +1,7 @@
+package test.week1;
+
+public class Numbers {
+    public static void main(String[] args) {
+
+    }
+}
