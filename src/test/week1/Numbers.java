9e58e6240542a900616261a2ea22e45dfb965814
Alec1 <alec0505@gmail.com>
2022-10-09 19:03:00 -0400

    test1 week1 task Numbers updated

--- src/test/week1/Numbers.java
+++ src/test/week1/Numbers.java
@@ -3,5 +3,17 @@ package test.week1;
 public class Numbers {
     public static void main(String[] args) {
 
+        divide(11,3);
+        System.out.println(divide(11,3));
+
+
+    }
+    public static int divide(int num1, int num2){
+        int count = 0;
+        for(int i = num2; i <= num1; i = i + num2){
+            count ++;
+
+        }
+        return  count;
     }
 }
